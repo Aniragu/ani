50e1ce858327782cf1241fd558345645d45cc715
Aniragu <aniragu22@gmail.com>
2017-04-18 14:43:04 +0530

    Add files via upload

---
+++ ani.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int n,m;
+printf("\nEnter the number");
+scanf("%d",&n);
+if(n>0)
+printf("\n the given number is positive");
+else if(n<0)
+printf("\n the given number is negative");
+else
+printf("\n zero");
+return 0;
+}
