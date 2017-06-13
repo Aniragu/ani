8021c24b6e85b3ee3b156be94b65cd75e92fdb52
Aniragu <aniragu22@gmail.com>
2017-06-13 15:20:39 +0530

    Add files via upload

--- Ascii.java
+++ Ascii.java
@@ -6,17 +6,12 @@ public class Ascii {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-        String ch;
-        int i;
-        for(i=1; i<=255; i++)
-        {
-            ch = new Character((char)i).toString();
-            System.out.print(i+ " -> " + ch + "\t");
-        }
 		
-    }
-
+		  int i;
+	        for(i = 0; i < 26; i++){
+	            System.out.println((char)('A'+i) + " --> " + ('A'+i));
+	            
+	         }
+	    }
 	}
-
 
