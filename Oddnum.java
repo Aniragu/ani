0e763217fe92827ec803dd8744d2e2faf0aa7746
Aniragu <aniragu22@gmail.com>
2017-06-13 15:27:45 +0530

    Add files via upload

---
+++ Oddnum.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+public class Oddnum {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            int i;
+            Scanner ss=new Scanner(System.in);
+            for(i=1;i<=100;i++){
+            	if(i%2!=0){
+            		System.out.println(i);
+            	}
+            }
+            
+	}
+
+}
