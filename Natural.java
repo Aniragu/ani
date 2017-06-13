bf9a403fdd8b772fd964c0a6d614ce60d0d627ad
Aniragu <aniragu22@gmail.com>
2017-06-13 11:54:12 +0530

    Add files via upload

---
+++ Natural.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Natural {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int x, i = 1 ;
+
+        int sum = 0;
+
+        System.out.println("Enter Number of items :");
+
+        Scanner s = new Scanner(System.in);
+
+        x = s.nextInt();
+
+        while(i <= x)
+
+        {
+
+            sum = sum +i;
+
+            i++;
+
+        }
+
+        System.out.println("Sum of "+x+" numbers is :"+sum);
+
+    } 
+
+
+	}
+
+
