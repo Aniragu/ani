66b398c0856fb7d6bd21697ec4567b80d6145fbc
Aniragu <aniragu22@gmail.com>
2017-06-13 10:08:32 +0530

    Add files via upload

---
+++ Evennumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Evennumbers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            int i;
+            Scanner ss=new Scanner(System.in);
+            for(i=23;i<=57;i++){
+            	if(i%2==0){
+            		System.out.println(i);
+            	}
+            }
+            
+	}
+
+}
