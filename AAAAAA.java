aee546e35ac3bc81c517ad86c7eeb0bcf2ce0d08
Aniragu <aniragu22@gmail.com>
2017-06-14 12:14:11 +0530

    Add files via upload

--- AAAAAA.java
+++ AAAAAA.java
@@ -1,50 +1,22 @@
-
-public class AAAAAA {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 int maxRows = 3;
-		    int maxCols = 4;
-
-		    int [] onedArray = new int [maxRows];
-		        for (int i = 0; i < maxRows; i++){
-		        onedArray[i] = (int) ((Math.random() * 100) * maxCols);
-		    }
-
-		    int [][] twodArray = new int[maxRows][];
-		        for (int i = 0; i < maxRows; i++){
-		        twodArray[i] = new int[maxCols];
-		    }
-
-		        for (int i = 0; i < twodArray.length; i++){
-		        for (int j = 0; j < twodArray[i].length; j++){
-		            twodArray[i][j] = (int) (Math.random() * 100);
-		        }
-		    }
-
-		    System.out.println("2 - The 2D array: ");
-		    for (int i = 0; i < twodArray.length; i++){
-		        for (int j = 0; j < twodArray[i].length; j++){
-		            System.out.print(twodArray[i][j] + " ");
-	}
-		        System.out.println("");
-		    }
-		    int maxValue = 1;
-		    System.out.println("\nMax values in 2D array: ");
-		    for (int i = 0; i < twodArray.length; i++) {
-		        for (int j = 0; j < twodArray.length; j++)
-		        if (twodArray[i][j] > maxValue) {
-		        maxValue = twodArray[i][j];
-		        }
-		            System.out.println(maxValue);break; 
-		        }
-
-
-
-		}
-	}
+import java.util.Scanner;
+public class AAAAAA{
+	 public static void main(String args[])throws Exception
+     {
+         Scanner sc = new Scanner(System.in);
+         System.out.print("Enter the First no : ");
+         int n1=sc.nextInt();
+         System.out.print("Enter the Second no : ");
+         int n2=sc.nextInt();
+          
+         while(n1 != n2)
+         {
+             if(n1 > n2)
+                 n1 = n1-n2;
+             else
+                 n2 = n2-n1;
+         }
+         System.out.print("GCD = "+n1);
+     }
+ }
 
 
