68fdd1a8b5bc36a904039b70970a76be31525817
Aniragu <aniragu22@gmail.com>
2017-06-16 15:32:34 +0530

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,13 @@
+
+public class Vehicle {
+
+	Vehicle(){
+		System.out.println("Bike is created");
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Vehicle v=new Vehicle();
+	}
+
+}
