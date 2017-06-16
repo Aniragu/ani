fda46dde9cfc728a06656cf01cabf757f0fca625
Aniragu <aniragu22@gmail.com>
2017-06-16 09:47:09 +0530

    Add files via upload

---
+++ Calc.java
@@ -0,0 +1,13 @@
+
+public class Calc {
+	 
+		  void sum(int a,int b){System.out.println(a+b);}  
+		  void sum(int a,int b,int c){System.out.println(a+b+c);}  
+		  
+		  public static void main(String args[]){  
+		  Calc obj=new Calc();  
+		  obj.sum(10,10,10);  
+		  obj.sum(20,20);  
+		  
+		  }  
+		} 
